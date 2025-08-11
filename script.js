00f22787c0c617fe486e69bf6ad4b9063857e6fd
Amahle <amahlez798@gmail.com>
2025-08-11 06:31:21 +0200

    Added a play button, doesn't work as yet, but the concept holds true in the javascript

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 player = document.getElementById('player');
+play_pause_button = document.getElementById('playpause');
 
 function hide() {
     if (player){
         player.style.visibility = "hidden";
     }
 }
+
+play_pause_button.addEventListener('click', function() {
+    console.log('Play!');
+});
 
 hide()
