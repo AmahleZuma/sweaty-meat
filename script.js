9efaf040bf3828e72df35552b7d6ab42ab5beadc
Amahle <amahlez798@gmail.com>
2025-08-13 11:25:52 +0200

    Can play and pause the audio

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-player = document.getElementById('player');
-play_pause_button = document.getElementById('playpause');
 
+player = document.getElementById('player'); // This is my audio element
+play_pause_button = document.getElementById('playpause'); // This is the play button
+
+// Function to hide the audio element
 function hide() {
     if (player){
         player.style.visibility = "hidden";
     }
 }
 
+// the function for playing and pausing the audio
 play_pause_button.addEventListener('click', function() {
-    console.log('Play!');
+    if (player.paused) { // audio will play
+        player.play();
+        play_pause_button.src = 'assets/pause.svg'
+        console.log('Play!');
+    } else { // audio will pause
+        player.pause();
+        play_pause_button.src = 'assets/play.svg'
+        console.log('Paused!');
+    };
+
+
+
+    
     
 });
 
-hide()
+play_pause_button.src = player.paused ? 'assets/play.svg' : 'assets/pause.svg' // Maintaining the default to play
+
+hide() // Hide the audio element...looks ghetto and cheap
+
